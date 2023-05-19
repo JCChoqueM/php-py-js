4433a1777731c2953c83799ecb9bd4deac2d0a1b
JCChoqueM <jcchoquem23@gmail.com>
2023-05-18 21:47:58 -0400

    as
    da

--- animation/animacionJS.js
+++
@@ -1,7 +0,0 @@
-const resultadoJS = document.querySelector(".resultado.javascript");
-gsap.to(resultadoJS, {
-  duration: 1 /* duracion de la animacion en segundos */,
-  y: 100, // Desplazamiento vertical en píxeles
-  opacity: 1, // Opacidad final del elemento (1 significa completamente visible)
-  ease: "power2.out", // Tipo de animación (puedes ver más opciones en la documentación de TweenMax)
-});
--- animation/animacionPY.js
+++
@@ -1,8 +0,0 @@
-/* NOTE animacion que baja al procesar resultado */
-const resultadoPY = document.querySelector(".resultado.python");
-gsap.to(resultadoPY, {
-  duration: 1, // Duración de la animación en segundos
-  y: 100, // Desplazamiento vertical en píxeles
-  opacity: 1, // Opacidad final del elemento (1 significa completamente visible)
-  ease: "power2.out", // Tipo de animación (puedes ver más opciones en la documentación de TweenMax)
-});
