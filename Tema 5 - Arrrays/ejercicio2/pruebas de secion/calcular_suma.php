c98481fab5bc66aad9c45931df3edeb46a1cb93e
JCChoqueM <jcchoquem23@gmail.com>
2023-08-29 13:44:53 -0400

    wfdsf
    sdfsd

--- Tema 5 - Arrrays/ejercicio2/pruebas de secion/calcular_suma.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_GET["numeros"])) {
-    $numeros = json_decode($_GET["numeros"]);
-    
-    if (count($numeros) === 3) {
-        $suma = array_sum($numeros);
-        echo $suma;
-    }
-}
