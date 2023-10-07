636abcdd2972e4ccbbd36a540062306138ea77fa
JCChoqueM <jcchoquem23@gmail.com>
2023-10-07 03:19:25 -0400

    hg
    fh

--- Tema 5 - Arrrays/ejercicio3/2.php
+++ Tema 5 - Arrrays/ejercicio3/2.php
@@ -1,59 +1,34 @@
 <?php
-$n = $_GET['n'];
-$contadorNumeros = $_GET['contadorNumeros'];
-$numeroTexto = $_GET['numeroTexto'];
-
-if (!isset($n)) {
-    $contadorNumeros = 0;
-    $numeroTexto = "";
-}
-
-////////////////////////////////////////////////////////////////
-//  Programa principal una vez recogidos los datos del array.
-//  El array con los números es $numero
-////////////////////////////////////////////////////////////////
-
-if ($contadorNumeros == 10) {
-    $numeroTexto = $numeroTexto . " " . $n; // añade el último número leído
-    $numeroTexto = substr($numeroTexto, 2); // quita espacios sobrantes
-    $numero = explode(" ", $numeroTexto);
-
-    $maximo = -PHP_INT_MAX;
-    $minimo = PHP_INT_MAX;
-
-    foreach ($numero as $n) {
-        if ($n < $minimo) {
-            $minimo = $n;
-        }
-        if ($n > $maximo) {
-            $maximo = $n;
-        }
+if (!isset($_GET['numeroTexto'])) {
+    // Genera el array aleatorio
+    for ($i = 0; $i < 100; $i++) {
+        $numero[$i] = rand(0, 20);
+        echo $numero[$i] . " ";
     }
 
-    foreach ($numero as $n) {
-        if ($n == $minimo) {
-            echo "$n mínimo<br>";
-        } else if ($n == $maximo) {
-            echo "$n máximo<br>";
-        } else {
-            echo "$n<br>";
-        }
-    }
-}
-////////////////////////////////////////////////////////////////
-
-
-
-// Pide número y añade el actual a la cadena
-if (($contadorNumeros < 10) || (!isset($n))) {
+    $numeroTexto = implode(" ", $numero);
 ?>
+    <br><br>
     <form action="2.php" method="get">
-        <input type="hidden" name="ejercicio" value="02">
-        Introduzca un número:
-        <input type="number" name="n" autofocus>
-        <input type="hidden" name="contadorNumeros" value="<?= ++$contadorNumeros ?>">
-        <input type="hidden" name="numeroTexto" value="<?= $numeroTexto . " " . $n ?>">
+        <input type="hidden" name="ejercicio" value="04">
+        Valor a sustituir: <input type="number" name="viejo" autofocus="" required=""><br>
+        Valor nuevo: <input type="number" name="nuevo" required="">
+        <input type="hidden" name="numeroTexto" value="<?php echo $numeroTexto; ?>">
         <input type="submit" value="OK">
     </form>
 <?php
+} else {
+    $numeroTexto = $_GET['numeroTexto'];
+    $viejo = $_GET['viejo'];
+    $nuevo = $_GET['nuevo'];
+
+    $numero = explode(" ", $numeroTexto);
+
+    foreach ($numero as $n) {
+        if ($n == $viejo) {
+            echo "<span style=\"color: green; font-weight: bold;\">$nuevo</span> ";
+        } else {
+            echo  "$n ";
+        }
+    }
 }
