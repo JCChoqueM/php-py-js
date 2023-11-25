9f5ab590f208ffb9de5a6f273868aec0ce26386c
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2023-11-25 03:52:16 -0400

    asd
    asd

--- Tema 5 - Arrrays/ejercicio9/js/generarNumero.js
+++ Tema 5 - Arrrays/ejercicio9/js/generarNumero.js
@@ -5,7 +5,14 @@ function generarNumero(max, min) {
 function generarNumeroAleatorio() {
   const num1 = document.getElementById("num1");
 
-  num1.value = generarNumero(100, -2);
+  num1.value = generarNumero(20, 51);
+}
+function generarInicialFinal() {
+  let numInicial = generarNumero(0, 5);
+  let numFinal = generarNumero(0, 5);
+
+  document.getElementById("initial").value = numInicial;
+  document.getElementById("final").value = numFinal;
 }
 
 /* !SECTION fin Numeros random */
