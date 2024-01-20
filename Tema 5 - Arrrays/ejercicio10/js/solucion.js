fef0c0b640a0a9f422bd439f811d5b37d4d284d8
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2024-01-20 04:39:34 -0400

    asd
    asd

--- Tema 5 - Arrrays/ejercicio10/js/solucion.js
+++ Tema 5 - Arrrays/ejercicio10/js/solucion.js
@@ -1,46 +1,48 @@
 /* SECTION inicio boton PHP */
-function solucionPHP_JS_PY(){
+function solucionPHP_JS_PY() {
   solucionPHP();
   solucionPython();
   solucionJavaScript();
 }
 function limpiarResultados() {
-  document.getElementById("resultadoPHP").innerHTML = "";
-  document.getElementById("resultadoPython").innerHTML = "";
-  document.getElementById("resultadoJavaScript").innerHTML = "";
+  document.getElementById("resultadoPHP").innerHTML = "Porque me borraste???";
+  document.getElementById("resultadoPython").innerHTML =
+    "Porque me borraste???";
+  document.getElementById("resultadoJavaScript").innerHTML =
+    "Porque me mataste BArt????";
 }
-function solucionPHP () {
+function solucionPHP() {
   /*  const num1 = document.getElementById("num1").value;
 
   const formData = new FormData();
   formData.append("num1", num1); */
 
-  fetch('archives/soluciones/solucion.php', {
+  fetch("archives/soluciones/solucion.php", {
     /*   method: "POST",
     body: formData, */
   })
-    .then(response => response.text())
-    .then(text => {
-      const resultadoDiv = document.getElementById('resultadoPHP')
-      resultadoDiv.innerHTML = text
+    .then((response) => response.text())
+    .then((text) => {
+      const resultadoDiv = document.getElementById("resultadoPHP");
+      resultadoDiv.innerHTML = text;
       /*            document.getElementById("form").reset(); */
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
 }
 /* !SECTION fin boton PHP */
 
 /* SECTION inicio boton Python */
-function solucionPython () {
+function solucionPython() {
   /*  const num1 = document.getElementById("num1").value; */
 
   // Enviar la solicitud al servidor utilizando fetch
-  fetch('archives/soluciones/solucionPy.php') //.php?num1=" + num1)+ "&metodo=python" || + "&num2=" + num2
-    .then(response => response.text())
-    .then(text => {
+  fetch("archives/soluciones/solucionPy.php") //.php?num1=" + num1)+ "&metodo=python" || + "&num2=" + num2
+    .then((response) => response.text())
+    .then((text) => {
       // Mostrar el resultado en la página
-      const resultadoDiv = document.getElementById('resultadoPython')
-      resultadoDiv.innerHTML = text
+      const resultadoDiv = document.getElementById("resultadoPython");
+      resultadoDiv.innerHTML = text;
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
 }
 /* !SECTION fin boton Python */
