0d443e60c4b02ecb7bd08a5b1b0dedfbb4173fa5
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2024-03-04 15:51:34 -0400

    funcines
    =)

---
+++ Tema 6 - Funciones/Ejercicio1-14/js/limpiarResultados.js
@@ -0,0 +1,5 @@
+function limpiarResultados() {
+  document.getElementById("resultadoPHP").innerHTML = "Ya no estoy, estas feliz?";
+  document.getElementById("resultadoPython").innerHTML = "Ya no estoy, estas feliz?";
+  document.getElementById("resultadoJavaScript").innerHTML = "Ya no estoy, estas feliz?";
+}
