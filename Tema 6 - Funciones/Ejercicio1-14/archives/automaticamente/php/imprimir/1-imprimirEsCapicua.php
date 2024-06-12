8ce91ee3182f38fec9158b7a1bd1c96b7c7a020a
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2024-06-11 23:59:15 -0400

    12
    12

--- Tema 6 - Funciones/Ejercicio1-14/archives/automaticamente/php/imprimir/1-imprimirEsCapicua.php
+++ Tema 6 - Funciones/Ejercicio1-14/archives/automaticamente/php/imprimir/1-imprimirEsCapicua.php
@@ -16,7 +16,7 @@ function imprimirEscapicua($input1)
                 case $esValido:
                     if ($input1 >= 0) {
                         $mensajeError =
-                            "<span>El numero dfgdfd<span style='color: green'>" . $input1 . "</span> es un palíndromo.</span>";
+                            "<span>El numero <span style='color: green'>" . $input1 . "</span> es un palíndromo.</span>";
                     } else {
                         $mensajeError = "<span>El número <span style='color: red'>" . $input1 . "</span>  es negativo<br>Al ignorar el signo:<br> <span style='color: green'>" . abs($input1) . " </span>es un palíndromo.</span>";
                     }
@@ -33,23 +33,6 @@ function imprimirEscapicua($input1)
         default:
             $mensajeError = "Error desconocido. Por favor, revise las entradas.";
     }
-    if (!is_numeric($input1)) {
-        $mensajeError = "El campo está vacío o no es un número. Por favor, ingrese número(s).";
-    } else {
-        $input1 = (int) $input1; // Convertir a entero
-        if ($esValido) {
-            if ($input1 >= 0) {
-                $mensajeError = "<span>El numero<span style='color: green'>" . $input1 . "</span> es un palíndromo.</span>";
-            } else {
-                $mensajeError = "<span>El número <span style='color: red'>" . $input1 . "</span>  es negativo<br>Al ignorar el signo:<br> <span style='color: green'>" . abs($input1) . " </span>es un palíndromo.</span>";
-            }
-        } else {
-            if ($input1 >= 0) {
-                $mensajeError = "<span>" . esCapicua($input1) . "</span>";
-            } else {
-                $mensajeError = "<span>El número <span style='color: red'>" . $input1 . "</span> es negativo.<br> Al ignorar el signo:<br> "  . esCapicua($input1) . "</span>";
-            }
-        }
-    }
+
     return $mensajeError;
 }
