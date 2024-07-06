c83af48c093089cbcf3b1ac1202512c5545c3d6a
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2024-07-06 01:16:45 -0400

    asdf
    asdf

--- Tema 6 - Funciones/Ejercicio1-14/archives/automaticamente/php/imprimir/9-imprimirQuitaPorDetras.php
+++ Tema 6 - Funciones/Ejercicio1-14/archives/automaticamente/php/imprimir/9-imprimirQuitaPorDetras.php
@@ -1,9 +1,7 @@
 <?php
 function imprimirQuitaPorDetras($input1, $input2)
 {
-    $input1 = intval($input1);
-    $input2 = intval($input2);
-    $nDigito = digitos($input1);
+    $nDigito = digitos(intval($input1));
     $centrar = "style='text-align: center;'";
     $rojo = "style='color: red;'";
     $verde = "style='color: darkgreen;'";
@@ -12,29 +10,33 @@ function imprimirQuitaPorDetras($input1, $input2)
 
     switch ($estado) {
         case "00":
-            $mensajeError = "00- Ambos campos están vacíos. Por favor, ingrese números.";
+            $mensajeError = "Ambos campos están vacíos. Por favor, ingrese números.";
             break;
         case "01":
-            $mensajeError = "01- El campo 1 está vacío.";
+            $mensajeError = "El campo 1 está vacío.";
             break;
         case "10":
-            $mensajeError = "10- El campo 2 está vacío.";
+            $mensajeError = "El campo 2 está vacío.";
             break;
         case "11":
-            if ($input2 > $nDigito) {
-                $mensajeError = "
-                <div $centrar>El número $input1 tiene <span $verde>$nDigito " . ($nDigito == 1 ? "dígito." : "dígitos.") . "</span> <br>
-                " . crearTablaReves($input1, 1) . "
-                <span $rojo>No se puede quitar $input2 dígitos por Detras</span>.
-                </div>";
+            $input1 = intval($input1);
+            $input2 = intval($input2);
+            $mensajeError = "<div $centrar>";
+            $mensajeError .= "El número $input1 tiene <span $verde>$nDigito " . ($nDigito == 1 ? "dígito." : "dígitos.") . "</span> <br>";
+            $mensajeError .= crearTablaReves(abs($input1), 1);
+            if ($input2 == 0) {
+                $mensajeError .= "<span $rojo>Quitar $input2 dígitos nos dara el mismo numero $input1</span><br>";
+                $mensajeError .= crearTablaReves(abs($input1), 1);
+            } elseif ($input2 > $nDigito) {
+                $mensajeError .= "<span $rojo>No se puede quitar $input2 dígitos por Detras</span>";
             } elseif ($input2 < 0) {
-                $mensajeError = "<span $rojo>La cantidad de números a quitar no puede ser negativa</span>";
+                $mensajeError .= "<span $rojo>La cantidad de números a quitar no puede ser negativa</span>";
+            } elseif ($input2 == $nDigito) {
+                $mensajeError .= "<span $rojo>Quitar $input2 dígitos resultará en 0</span>";
             } else {
-                $mensajeError = "Continuar:<br><div $centrar>El número $input1 tiene <span $verde>$nDigito " . ($nDigito == 1 ? "dígito." : "dígitos.") . "</span> <br>
-                " . crearTablaReves($input1, 1) . "
-                <span $verde>Se puede quitar $input2 dígitos por Detras</span>.
-                </div>";
+                $mensajeError .= "<span $verde>Se puede quitar $input2 dígitos por Detras</span>.";
             }
+            $mensajeError .= "</div>";
             break;
         default:
             $mensajeError = "Error desconocido. Por favor, revise las entradas.";
