88ff3fc30a1e82f768d0c79fb4033c60269606eb
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2024-08-11 16:23:08 -0400

    nuevo menu completo
    nuevo menu completo

--- Tema 6 - Funciones/ejercicios 20-28/js/llenarPagina/cargarInput.js
+++ Tema 6 - Funciones/ejercicios 20-28/js/llenarPagina/cargarInput.js
@@ -3,35 +3,50 @@ function cargarInput() {
   var select = document.getElementById("accion");
   var opcionSeleccionada = select.options[select.selectedIndex].value;
 
-  // Cargar el contenido de inputs.html utilizando fetch
-  fetch("inputs.html")
-    .then((response) => response.text())
-    .then((data) => {
-      // Crear un elemento HTML temporal para manipular el contenido
-      var tempDiv = document.createElement("div");
-      tempDiv.innerHTML = data;
+  // Definir las opciones especiales
+  var opcionesEspeciales = [
+    "estaEnArrayInt",
+    "posicionEnArrayInt",
+    "rotaDerechaArrayInt",
+    "rotaIzquierdaArrayInt",
+  ];
 
-      // Obtener el elemento específico por su ID según la opción seleccionada
-      var inputContainer = tempDiv.querySelector(
-        "#" + opcionSeleccionada + "Input"
-      );
+  // Si la opción seleccionada está en las opciones especiales
+  if (opcionesEspeciales.includes(opcionSeleccionada)) {
+    // Cargar el contenido de inputs.html utilizando fetch
+    fetch("inputs.html")
+      .then((response) => response.text())
+      .then((data) => {
+        // Crear un elemento HTML temporal para manipular el contenido
+        var tempDiv = document.createElement("div");
+        tempDiv.innerHTML = data;
 
-      // Clonar el contenido del input container para evitar problemas de referencia
-      var inputContent = inputContainer.cloneNode(true);
+        // Obtener el elemento específico por su ID según la opción seleccionada
+        var inputContainer = tempDiv.querySelector(
+          "#" + opcionSeleccionada + "Input"
+        );
 
-      // Limpiar el contenedor de inputs antes de agregar el nuevo contenido
-      var funcionesInputContainer = document.getElementById("funcionesInput");
-      funcionesInputContainer.innerHTML = "";
+        // Clonar el contenido del input container para evitar problemas de referencia
+        var inputContent = inputContainer.cloneNode(true);
 
-      // Insertar el contenido clonado en el contenedor deseado
-      funcionesInputContainer.appendChild(inputContent);
-    })
-    .catch((error) => console.error("Error:", error));
+        // Limpiar el contenedor de inputs antes de agregar el nuevo contenido
+        var funcionesInputContainer = document.getElementById("funcionesInput");
+        funcionesInputContainer.innerHTML = "";
+
+        // Insertar el contenido clonado en el contenedor deseado
+        funcionesInputContainer.appendChild(inputContent);
+      })
+      .catch((error) => console.error("Error:", error));
+  } else {
+    // Si la opción seleccionada no está en las opciones especiales, no hacer nada
+    var funcionesInputContainer = document.getElementById("funcionesInput");
+    funcionesInputContainer.innerHTML = "";
+  }
 }
 
 // Llamar a la función cargarInput al cargar la página para mostrar el primer input
 function inicializar() {
-  document.getElementById("accion").value = "minimoArrayInt";
+  document.getElementById("accion").value = "estaEnArrayInt";
   cargarInput();
   cargarTooltip();
   cambiarColor();
