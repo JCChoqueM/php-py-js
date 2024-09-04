ead64861d1bf3e4e8cfd5a93ae403e44e8b3c3ca
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2024-09-04 02:47:26 -0400

    acabado
    acabado

--- Tema 6 - Funciones/ejercicios 20-28/archives/js/funciones/6-posicionEnArrayInt.js
+++ Tema 6 - Funciones/ejercicios 20-28/archives/js/funciones/6-posicionEnArrayInt.js
@@ -1,9 +1,16 @@
 function posicionEnArrayInt(input1, extra) {
   let posiciones = [];
+  console.log(".......................")
+  console.log(input1);
+          console.log(extra);
+
 extra= parseInt(extra)
+console.log(["2",4,"hola","4"])
   // Verifica si input1 es un array
   if (Array.isArray(input1)) {
     input1.forEach((valor, indice) => {
+
+
       if (valor === extra) {
         posiciones.push(indice);
       }
