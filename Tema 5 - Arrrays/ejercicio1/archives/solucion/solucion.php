9e919c96718971e48c3204a27f2ef0253d56b7c0
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2024-12-30 01:19:54 -0400

    asdf
    asdf

--- Tema 5 - Arrrays/ejercicio1/archives/solucion/solucion.php
+++ Tema 5 - Arrrays/ejercicio1/archives/solucion/solucion.php
@@ -7,7 +7,7 @@ foreach ($numero as $elemento) {
   $cuadrado[] = pow($elemento, 2);
   $cubo[] = pow($elemento, 3);
 }
-$mensaje .= " <table border='1'><tr><th>Nº</th><th>Numero</th><th>Cuadrado</th><th>Cubo</th></tr>";
+$mensaje .= " <table border='1'><tr><th>Nº</th><th>Numero</th><th>Cuadradito</th><th>Cubo</th></tr>";
 for ($contador = 0; $contador < 6; $contador++) {
   $mensaje .= "<tr>
   <td>" . ($contador + 1) . "</td>
